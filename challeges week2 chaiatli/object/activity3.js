f9c030485dd549b499c1a38dd1bc92d9399a04aa
chaitali <83662227+tutor723@users.noreply.github.com>
2023-07-08 16:53:53 +0100

    activity3.js

--- challeges week2 chaiatli/object/activity3.js
+++ challeges week2 chaiatli/object/activity3.js
@@ -1,63 +1,41 @@
 
 //activity3 object
 const coffeeShop = {
-  branch: "Main Street",
-  drinks: {
-    "Espresso": 2.5,
-    "Cappuccino": 3.0,
-    "Latte": 3.5,
-    "Mocha": 4.0
-  },
-  food: {
-    "Croissant": 2.0,
-    "Bagel": 2.5,
-    "Sandwich": 4.0,
-    "Cake": 3.5
-  },
-  drinksOrdered: function(order) {
-    let totalCost = 0;
-    let orderSummary = "Drinks ordered:\n";
+    branchName: "Bob's Coffee Shop",
 
-    for (const drink in order) {
-      if (this.drinks.hasOwnProperty(drink)) {
-        const price = this.drinks[drink];
-        orderSummary += `${drink} - $${price}\n`;
-        totalCost += price;
-      }
+    drinkMenu: ["Tea", 1.50, "Coffee", 2.30],
+    foodMenu: ["Pizza", 2.75, "Cheese", 3.45],
+
+    drinkOrder(drink){
+        for (let i = 0; i<this.drinkMenu.length; i++){
+            if(drink===this.drinkMenu[i]){
+                return this.drinkMenu[i+1]
+            }
+        } 
+        return false 
+    },
+
+    foodOrder(food){
+        for (let i = 0; i<this.foodMenu.length; i++){
+            if(food===this.foodMenu[i]){
+                let foodPrice = this.foodMenu[i+1]
+                return this.foodMenu[i+1]
+            } 
+        }
+        return false
+
+    },
+
+    order(drink, food){
+        if (this.foodOrder(food) && this.drinkOrder(drink)){
+            console.log(`You have ordered ${drink} at ${this.drinkOrder(drink)}`)
+            console.log(`You have ordered ${food} at ${this.foodOrder(food)}`)
+            console.log(`Total cost £${this.foodOrder(food)+ this.drinkOrder(drink)}`)
+
+    } else {
+            console.log("We do not have that item")
     }
+}
+}
 
-    orderSummary += `Total cost: $${totalCost.toFixed(2)}`;
-    return orderSummary;
-  },
-  foodOrdered: function(order) {
-    let totalCost = 0;
-    let orderSummary = "Food ordered:\n";
-
-    for (const foodItem in order) {
-      if (this.food.hasOwnProperty(foodItem)) {
-        const price = this.food[foodItem];
-        orderSummary += `${foodItem} - $${price}\n`;
-        totalCost += price;
-      }
-    }
-
-    orderSummary += `Total cost: $${totalCost.toFixed(2)}`;
-    return orderSummary;
-  }
-};
-
-
-You can use the drinksOrdered and foodOrdered methods to get a summary of the ordered items and their costs. Here's an example usage:
-const myDrinkOrder = {
-  "Espresso": 2,
-  "Latte": 1
-};
-
-console.log(coffeeShop.drinksOrdered(myDrinkOrder));
-
-const myFoodOrder = {
-  "Croissant": 2,
-  "Cake": 1
-};
-
-console.log(coffeeShop.foodOrdered(myFoodOrder));
+coffeeShop.order("Coffee", "Pizza")
