81e657e5182c44646592f45089ba57148baa995a
chaitali <83662227+tutor723@users.noreply.github.com>
2023-07-08 16:49:47 +0100

    Update activity3.js

--- challeges week2 chaiatli/object/activity3.js
+++ challeges week2 chaiatli/object/activity3.js
@@ -1,30 +1,63 @@
 
 //activity3 object
-let coffeeShop  ={
-    branch:"main", 
-    cocaCola : 5,
-   
-    chips:5 ,
-    
-   
-    drinksOrdered(){
-  
-  return `Your  drink order cost  is with all items chosen with costs ${this.cocaCola} pounds `
-    },
+const coffeeShop = {
+  branch: "Main Street",
+  drinks: {
+    "Espresso": 2.5,
+    "Cappuccino": 3.0,
+    "Latte": 3.5,
+    "Mocha": 4.0
+  },
+  food: {
+    "Croissant": 2.0,
+    "Bagel": 2.5,
+    "Sandwich": 4.0,
+    "Cake": 3.5
+  },
+  drinksOrdered: function(order) {
+    let totalCost = 0;
+    let orderSummary = "Drinks ordered:\n";
 
-    foodOrdered (){
-        return `your food  order cost is with all items chosen with costs  ${this.chips}  pounds`
+    for (const drink in order) {
+      if (this.drinks.hasOwnProperty(drink)) {
+        const price = this.drinks[drink];
+        orderSummary += `${drink} - $${price}\n`;
+        totalCost += price;
+      }
     }
-,
-    totalCost(){
-        let cost = this.cocaCola+  this.chips;
-        return `The total cost is ${cost}  pounds`
-      
+
+    orderSummary += `Total cost: $${totalCost.toFixed(2)}`;
+    return orderSummary;
+  },
+  foodOrdered: function(order) {
+    let totalCost = 0;
+    let orderSummary = "Food ordered:\n";
+
+    for (const foodItem in order) {
+      if (this.food.hasOwnProperty(foodItem)) {
+        const price = this.food[foodItem];
+        orderSummary += `${foodItem} - $${price}\n`;
+        totalCost += price;
+      }
     }
-   
-}
+
+    orderSummary += `Total cost: $${totalCost.toFixed(2)}`;
+    return orderSummary;
+  }
+};
 
 
-console.log(coffeeShop. drinksOrdered());
-console.log(coffeeShop.foodOrdered());
-console.log(coffeeShop. totalCost());
+You can use the drinksOrdered and foodOrdered methods to get a summary of the ordered items and their costs. Here's an example usage:
+const myDrinkOrder = {
+  "Espresso": 2,
+  "Latte": 1
+};
+
+console.log(coffeeShop.drinksOrdered(myDrinkOrder));
+
+const myFoodOrder = {
+  "Croissant": 2,
+  "Cake": 1
+};
+
+console.log(coffeeShop.foodOrdered(myFoodOrder));
